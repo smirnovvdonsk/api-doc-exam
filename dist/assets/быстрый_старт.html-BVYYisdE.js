27add079bce37438727ed1b814bb23f4ec16aeed
asmirnov <asmirnov@mobiledep.ru>
2026-05-02 19:49:34 +0300

    add bundle

---
+++ dist/assets/быстрый_старт.html-BVYYisdE.js
@@ -0,0 +1,34 @@
+import{_ as i,c as l,a as n,f as t,e,b as u,w as o,r,o as d}from"./app-CiEoFJNa.js";const p={};function c(q,s){const a=r("RouteLink");return d(),l("div",null,[s[5]||(s[5]=n("h1",{id:"быстрыи-старт",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#быстрыи-старт"},[n("span",null,"Быстрый старт")])],-1)),n("ol",null,[s[3]||(s[3]=n("li",null,"Зарегистрируйтесь в личном кабинете.",-1)),n("li",null,[s[1]||(s[1]=e("Получите ")),u(a,{to:"/parts/user/%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F.html"},{default:o(()=>s[0]||(s[0]=[e("API-токен")])),_:1}),s[2]||(s[2]=e(". Токен позволит получить доступ к <Название сервиса> API. Система токенов позволит вам контролировать, кто и как взаимодействует с вашими данными через API."))]),s[4]||(s[4]=n("li",null,"Отправьте первый запрос. Для создания нового пользователя:",-1))]),s[6]||(s[6]=t(`<ul><li>выберите метод POST</li><li>заполните тело запроса, как показано ниже</li><li>нажмите <strong>Execute</strong></li></ul><div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre><code><span class="line">{</span>
+<span class="line">  &quot;username&quot;: &quot;Sema&quot;,</span>
+<span class="line">  &quot;email&quot;: &quot;&lt;произвольный email&gt;&quot;,</span>
+<span class="line">  &quot;firstName&quot;: &quot;Семен&quot;,</span>
+<span class="line">  &quot;lastName&quot;: &quot;Семенов&quot;,</span>
+<span class="line">  &quot;address&quot;: {</span>
+<span class="line">    &quot;country&quot;: &quot;РФ&quot;,</span>
+<span class="line">    &quot;city&quot;: &quot;Москва&quot;,</span>
+<span class="line">    &quot;street&quot;: &quot;Тверская&quot;,</span>
+<span class="line">    &quot;house&quot;: &quot;1&quot;</span>
+<span class="line">  },</span>
+<span class="line">  &quot;age&quot;: 15,</span>
+<span class="line">  &quot;isEmployee&quot;: true,</span>
+<span class="line">  &quot;accountStatus&quot;: &quot;active&quot;</span>
+<span class="line">}</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>В случае успешного выполнения запроса будет выведено сообщение</p><div class="language-text line-numbers-mode" data-highlighter="prismjs" data-ext="text"><pre><code><span class="line">201 	Пользователь успешно создан</span>
+<span class="line"></span>
+<span class="line">{</span>
+<span class="line">  &quot;id&quot;: &lt;id пользователя&gt;,</span>
+<span class="line">  &quot;username&quot;: &quot;string&quot;,</span>
+<span class="line">  &quot;email&quot;: &quot;string&quot;,</span>
+<span class="line">  &quot;firstName&quot;: &quot;string&quot;,</span>
+<span class="line">  &quot;lastName&quot;: &quot;string&quot;,</span>
+<span class="line">  &quot;address&quot;: {</span>
+<span class="line">    &quot;country&quot;: &quot;string&quot;,</span>
+<span class="line">    &quot;city&quot;: &quot;string&quot;,</span>
+<span class="line">    &quot;street&quot;: &quot;string&quot;,</span>
+<span class="line">    &quot;house&quot;: &quot;string&quot;</span>
+<span class="line">  },</span>
+<span class="line">  &quot;age&quot;: 0,</span>
+<span class="line">  &quot;isEmployee&quot;: true,</span>
+<span class="line">  &quot;accountStatus&quot;: &quot;active&quot;</span>
+<span class="line">}</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4))])}const v=i(p,[["render",c]]),b=JSON.parse('{"path":"/parts/user/%D0%B1%D1%8B%D1%81%D1%82%D1%80%D1%8B%D0%B9_%D1%81%D1%82%D0%B0%D1%80%D1%82.html","title":"Быстрый старт","lang":"ru-RU","frontmatter":{},"git":{"updatedTime":1777738679000,"contributors":[{"name":"asmirnov","username":"asmirnov","email":"asmirnov@mobiledep.ru","commits":1,"url":"https://github.com/asmirnov"}],"changelog":[{"hash":"85fecdf6d0906f85ae9061a38b88cb889a3f17fa","time":1777738679000,"email":"asmirnov@mobiledep.ru","author":"asmirnov","message":"init"}]},"filePathRelative":"parts/user/быстрый_старт.md"}');export{v as comp,b as data};
